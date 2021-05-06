cc33ee5322a6004bb96f850319c8b9c1d80b110b
Ankita <ankita@dcirrus.co.in>
2021-05-06 17:49:40 +0530

    push

--- src/connector/src/main/java/io/grpcweb/CorsFilter.java
+++ src/connector/src/main/java/io/grpcweb/CorsFilter.java
@@ -19,14 +19,14 @@ package io.grpcweb;
 // import java.io.IOException;
 // import javax.servlet.Filter;
 // import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.StringUtils;
-import com.google.common.net.HttpHeaders;
+// import javax.servlet.FilterConfig;
+// import javax.servlet.ServletException;
+// import javax.servlet.ServletRequest;
+// import javax.servlet.ServletResponse;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import org.apache.commons.lang3.StringUtils;
+// import com.google.common.net.HttpHeaders;
 
 public class CorsFilter implements Filter {
   @Override
